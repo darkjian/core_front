83ae01609e0d47ba69c9c67ba6535b8d33758b69
Kim Petorosyan <work@MacBook-Pro-Kim.local>
2025-12-20 19:27:36 +0400

    смена темы

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -29,10 +29,11 @@ export const ThemeChanger = () => {
     <Switch
       checked={isDark}
       onCheckedChange={handleThemeChange}
+      size='md'
       className='absolute top-4 right-4'
       aria-label='Toggle theme'
     >
-      {isDark ? <Sun className='h-3 w-3 text-yellow-500' /> : <Moon className='h-3 w-3 text-slate-700' />}
+      {isDark ? <Sun className='h-4 w-4 text-yellow-500' /> : <Moon className='h-4 w-4 text-slate-700' />}
     </Switch>
   )
 }
